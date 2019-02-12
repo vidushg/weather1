da65a39a8cd6ff4ef836b2c40857f71f201e7bd9
vidush <goswamiv@grinnell.edu>
2019-02-11 21:37:22 -0600

    Updated for testing

--- test_weather.py
+++ test_weather.py
@@ -1,7 +1,7 @@
 #tes_weather.py
 
 import pytest
-import weather 
+import weathertest 
 
 #locurl="http://api.ipstack.com/check?access_key=01ba0bb834c723d9d47f061780376e45"
 
@@ -9,36 +9,32 @@ import weather
 
 def test_temp1():
     ts = 0
-    val = weather.temp_conv(ts)
+    val = weathertest.temp_conv(ts)
     exp = -459
     assert int(val) == exp
 
 def test_temp2():
     ts = -2000
-    val = weather.temp_conv(ts)
+    val = weathertest.temp_conv(ts)
     exp = -4059
     assert int(val) == exp
 
 def test_temp3():
     ts = 30000
-    val = weather.temp_conv(ts)
+    val = weathertest.temp_conv(ts)
     exp = 53540
     assert int(val) == exp
 
 def test_temp4():
     ts = 0.0
-    val = weather.temp_conv(ts)
+    val = weathertest.temp_conv(ts)
     exp = -459
     assert int(val) == exp
 
 def test_temp5():
     ts = -2000.3
-    val = weather.temp_conv(ts)
+    val = weathertest.temp_conv(ts)
     exp = -4059
     assert int(val) == exp
 
 
-
-#def teardown_method(self, method):
- #       # This method is being called after each test case, and it will revert input back to original function
-  #      weather.input = input  
--- weather.py
+++ weather.py
@@ -8,9 +8,12 @@ import json
 url = "http://api.openweathermap.org/data/2.5/weather?appid=d0c61e2af77bea9ba7f337ad3f0495d6&q="
 locurl="http://api.ipstack.com/check?access_key=01ba0bb834c723d9d47f061780376e45"
 
+
+#Function takes temp in Kelvin and returns in Fahrenheit
 def temp_conv(val):
 	return  1.8*(val-273) + 32
 
+#Function takes in choice, and assigns name to cityname accordingly
 def assign_cityname(ch):
 	if ch == "2":
 
@@ -19,22 +22,21 @@ def assign_cityname(ch):
 
 		return locationobj['city']
 	elif ch == "1":
-		#print (city)
-		#cityname = city
+
 		str = input("Where are you? ")
 		return str
 
 	else :
-		#print(choice)
+
 		print("Incorrect input")
 		exit
 
+#main function
 def main():
 
 	print("Do you wish to enter a city or use current location? \n Press 1 for city and 2 for current\n")
 	choice = input("")
-	#choice = "1"
-	#cityname = ""
+
 	cityname = assign_cityname(choice)
 
 	if cityname!="":
@@ -42,6 +44,7 @@ def main():
 		response = requests.get(final)
 		jsonobject = response.json()
 		#print (jsonobject)
+		#response returns code 404 if city doesn't exist
 		if jsonobject["cod"] != "404":
 				info = jsonobject["main"]
 				temp = info["temp"]
